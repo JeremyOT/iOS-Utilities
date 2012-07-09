d74455b5d56748adf04f52b3f778793f409262dd
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-07-09 00:47:26 -0700

    updated debuglog to check for DEBUG def instead of DEBUG_LOG

--- DebugLog.h
+++ DebugLog.h
@@ -7,17 +7,17 @@
 
 #import <Foundation/Foundation.h>
 
-// Define DEBUG_LOG here or configure it as a preprocessor macro in your project settings. Using
+// Define DEBUG here or configure it as a preprocessor macro in your project settings. Using
 // project settings allows the log to automatically be disabled for release builds.
 
-#ifdef DEBUG_LOG
+#ifdef DEBUG
 #define DLog(...) \
 [[DebugLog defaultLog] log:[NSString stringWithFormat:@"%s (%@: %d): %@", __PRETTY_FUNCTION__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:__VA_ARGS__]]]
 #else
 #define DLog(...)
 #endif
 
-#ifdef DEBUG_LOG
+#ifdef DEBUG
 #define DLogThread(...) \
 [[DebugLog defaultLog] log:[NSString stringWithFormat:@"%s (%@: %d) %@: %@", __PRETTY_FUNCTION__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSThread currentThread], [NSString stringWithFormat:__VA_ARGS__]]]
 #else
