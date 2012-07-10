b53710d45c81cd44e0e36571a1322166f9be1ad9
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2012-07-09 23:42:59 -0700

    added parameter names to method signatures

--- JSONService.h
+++ JSONService.h
@@ -9,14 +9,14 @@
                method:(NSString*)method
               headers:(NSDictionary*)headers
            jsonString:(NSString*)body
-       receiveHandler:(void (^)(id, NSNumber*, NSDictionary*))receiveHandler
-         errorHandler:(void (^)(NSError*))errorHandler;
+       receiveHandler:(void (^)(id response, NSNumber *status, NSDictionary *headers))receiveHandler
+         errorHandler:(void (^)(NSError *error))errorHandler;
 
 -(void)requestWithURL:(NSURL*)url
                method:(NSString*)method
               headers:(NSDictionary*)headers
              jsonData:(id)body
-       receiveHandler:(void (^)(id, NSNumber*, NSDictionary*))receiveHandler
-         errorHandler:(void (^)(NSError*))errorHandler;
+       receiveHandler:(void (^)(id response, NSNumber *status, NSDictionary *headers))receiveHandler
+         errorHandler:(void (^)(NSError *error))errorHandler;
 
 @end
